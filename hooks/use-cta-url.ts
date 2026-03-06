69861c3cc860ff86a5ac6144589af69592fe81d2
Luan Oak <99412966+luancarvalho7@users.noreply.github.com>
2026-03-06 14:35:45 +0000

    feat: New CTA Url

--- hooks/use-cta-url.ts
+++ hooks/use-cta-url.ts
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 
-const BASE_URL = 'https://atom-consultoria-financeira-new.vercel.app/'
+const BASE_URL = 'https://share.hsforms.com/1Fg2hEt7fQZacb4xO___NZgspo1z'
 
 export function useCtaUrl() {
   const [url, setUrl] = useState(BASE_URL)
